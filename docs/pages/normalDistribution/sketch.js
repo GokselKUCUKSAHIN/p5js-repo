4c8ce26737440ab9868a60a3f8dc8a3178bfc350
Jellybeanci <gokselkucuksahin@gmail.com>
2021-02-19 03:06:48 +0300

    Yeni proje oluşturuldu

--- docs/pages/normalDistribution/sketch.js
+++ docs/pages/normalDistribution/sketch.js
@@ -1,11 +1,12 @@
+/// <reference path="../../typeDef/p5.global-mode.d.ts" />
+
 const size = 400;
 
 function setup() {
   const canvas = createCanvas(size, size);
   canvas.parent("p5canvas");
-  background(220);
+  background(51);
 }
 
 function draw() {
-
 }
