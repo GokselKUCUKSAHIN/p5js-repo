c6ada466a61c76cb00ca89937234d1d682b8bceb
Jellybeanci <gokselkucuksahin@gmail.com>
2022-01-17 21:48:27 +0300

    modulo draw added.

--- docs/pages/modular-multipication/sketch.js
+++ docs/pages/modular-multipication/sketch.js
@@ -1,34 +1,57 @@
 const width = 800;
 const height = 800;
+const sliders = {
+  modulo: 0,
+  dotCount: 0
+};
 
 function setup() {
-  const center = createVector(width / 2, height / 2);
+  center = createVector(width / 2, height / 2);
   const canvas = createCanvas(width, height);
   canvas.parent("p5canvas");
   // stroke(0);
   // strokeWeight(2);
-  fill(color(255, 0, 0));
 
-  ellipse(center.x, center.y, 10, 10);
-
+  fill(color(255, 0, 0)); // RED
+  ellipse(center.x, center.y, 10);
   // MODULO
-  moduloSlider = select("#modulo");
-  moduloSlider.input(moduloEvent);
+  sliders.modulo = select("#modulo");
+  sliders.modulo.input(moduloEvent);
 
   // DOT COUNT
-  dotCountSlider = select("#dot-count");
-  dotCountSlider.input(dotCountEvent);
+  sliders.dotCount = select("#dot-count");
+  sliders.dotCount.input(dotCountEvent);
+
+  fill(color(255, 255, 0)); // YELLOW
+  // TEST
+  noLoop();
+}
+
+function calcEndPoint(centerVector, angleDegree, r) {
+  return createVector(centerVector.x + r * cos(angleDegree), centerVector.x + r * sin(angleDegree));
 }
 
 function draw() {
-  select('#modulo').value();
+
 }
 
 // EVENTS
 function moduloEvent() {
-  console.log("Modulo Event Triggred:", moduloSlider.value());
+  console.log("Modulo Event Triggred:", sliders.modulo.value());
 }
 
 function dotCountEvent() {
-  console.log("Dot Count Event Triggred:", dotCountSlider.value());
+  const sliderValue = sliders.dotCount.value();
+  console.log("Dot Count Event Triggred:", sliderValue);
+  drawDots(sliderValue);
+}
+
+function drawDots(dotCount) {
+  background(51);
+  const gap = TWO_PI / dotCount;
+  for (let i = -1.5707963267948966; i < 4.71238898038469; i += gap) {
+    const point = calcEndPoint(center, i, 300);
+    ellipse(point.x, point.y, 5);
+  }
+  redraw(1);
 }
