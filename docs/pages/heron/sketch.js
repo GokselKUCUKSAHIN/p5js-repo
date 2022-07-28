220670a636b9b02ad070588f60f848419c8bbcc7
gokselkucuksahin <gokselkucuksahin@gmail.com>
2022-07-28 19:52:26 +0300

    pin size increased.

--- docs/pages/heron/sketch.js
+++ docs/pages/heron/sketch.js
@@ -2,9 +2,9 @@ let w = window.innerWidth;
 let h = window.innerHeight;
 
 const pins = [
-  new Pin(60, 60, 10),
-  new Pin(300, 205, 10),
-  new Pin(100, 330, 10)
+  new Pin(60, 60, 20),
+  new Pin(300, 205, 20),
+  new Pin(100, 330, 20)
 ];
 
 function setup() {
